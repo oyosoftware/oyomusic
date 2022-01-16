c7f9b46fa23a4c36d29eaed2d57320418bbbb351
oYoSoftware <code@oyosoftware.nl>
2022-01-16 10:07:41 +0100

    Add files via upload

--- scripts/hasFiles.php
+++ scripts/hasFiles.php
@@ -7,12 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 error_reporting(E_ERROR);
 require_once('../settings.inc');
 
-$symaudiosource = "/#f";
-if (file_exists($symaudiosource)) {
-    rmdir($symaudiosource);
-}
-symlink($audiosource, $symaudiosource);
-
 $link = mysqli_connect($server, $username, $password, $database);
 $sql = "use " . $database;
 if (!mysqli_query($link, $sql))
@@ -20,16 +14,42 @@ if (!mysqli_query($link, $sql))
 
 mysqli_set_charset($link, "utf8");
 
+if (file_exists($audiosource)) {
+    $target = $audiosource;
+} else {
+    $target = $_SERVER["DOCUMENT_ROOT"] . $audiosource;
+}
+
+$sql = "select * from albums where id=$albumid";
+$result = mysqli_query($link, $sql);
+$row = mysqli_fetch_assoc($result);
+$folder = $row["Folder"];
+$target = $target . $folder;
+
+$symaudiosource = "album";
+if (file_exists($symaudiosource)) {
+    rmdir($symaudiosource);
+    unlink($symaudiosource);
+}
+symlink($target, $symaudiosource);
+
 $sql = "select * from albums inner join tracks on id=albumid where id=$albumid or boxsetid=$albumid";
 $result = mysqli_query($link, $sql);
 
 $filesexist = false;
 while ($row = mysqli_fetch_assoc($result)) {
-
-    $file = $symaudiosource . $row["Folder"] . '/' . $row["FileName"];
+    $file = $symaudiosource . '/' . $row["FileName"];
     $linkinfo = linkinfo($file);
 
-    fwrite($log, $file . " " . $linkinfo . "\r\n");
+    if ($linkinfo <> -1) {
+        $filesexist = true;
+        break;
+    }
+
+    $folder = $row["Folder"];
+    $pos = mb_strrpos($folder, "/");
+    $file = $symaudiosource . mb_substr($folder, $pos) . '/' . $row["FileName"];
+    $linkinfo = linkinfo($file);
 
     if ($linkinfo <> -1) {
         $filesexist = true;
@@ -42,4 +62,5 @@ echo $filesexist;
 mysqli_close($link);
 
 rmdir($symaudiosource);
+unlink($symaudiosource);
 ?>
