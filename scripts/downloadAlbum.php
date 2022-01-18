d60450c9f323cbc56a04d7912ee5431e4297e6d3
oYoSoftware <code@oyosoftware.nl>
2022-01-18 16:49:09 +0100

    Add files via upload

--- scripts/downloadAlbum.php
+++ scripts/downloadAlbum.php
@@ -9,12 +9,6 @@ require_once('../settings.inc');
 
 $log = fopen("output.log", "w");
 
-$symaudiosource = "/#z";
-if (file_exists($symaudiosource)) {
-    rmdir($symaudiosource);
-}
-symlink($audiosource, $symaudiosource);
-
 $link = mysqli_connect($server, $username, $password, $database);
 $sql = "use " . $database;
 if (!mysqli_query($link, $sql))
@@ -22,53 +16,73 @@ if (!mysqli_query($link, $sql))
 
 mysqli_set_charset($link, "utf8");
 
+if (file_exists($audiosource)) {
+    $target = $audiosource;
+} else {
+    $target = $_SERVER["DOCUMENT_ROOT"] . $audiosource;
+}
+
+$sql = "select * from artists inner join albums on artists.id=artistid where albums.id=$albumid";
+$result = mysqli_query($link, $sql);
+$row = mysqli_fetch_assoc($result);
+$name = $row["Name"];
+$released = $row["Released"];
+$title = $row["Title"];
+$folder = $row["Folder"];
+$target = $target . $folder;
+
+$symaudiosource = "download";
+if (file_exists($symaudiosource)) {
+    rmdir($symaudiosource);
+    unlink($symaudiosource);
+}
+symlink($target, $symaudiosource);
+
 $zip = new ZipArchive;
 $tempfile = tempnam("../tmp", "");
 $zip->open($tempfile, ZipArchive::CREATE);
+$zipfile = "$name - $released - $title.zip";
+
+$len = mb_strlen($folder);
+$pos = mb_strrpos($folder, "/");
+$offset = -1 * ($len - $pos + 1);
+$pos = mb_strrpos($folder, "/", $offset);
+
+$singlefolder = mb_substr($folder, $pos + 1);
+$singlelen = mb_strrpos($singlefolder, "/");
+$singlefolder = mb_substr($singlefolder, 0, $singlelen);
+if ($singlefolder == "Singles") {
+    $offset = -1 * ($len - $pos + 1);
+    $pos = mb_strrpos($folder, "/", $offset);
+}
+
+$sympos = mb_strlen($folder);
 
 $sql = "select * from albums where id=$albumid or boxsetid=$albumid order by boxsetindex";
 $result = mysqli_query($link, $sql);
 
-$isboxset = false;
+$counter = 0;
 while ($row = mysqli_fetch_assoc($result)) {
-
+    $counter++;
     $id = $row["Id"];
-    $artistid = $row["ArtistId"];
-    $released = $row["Released"];
-    $title = $row["Title"];
     $folder = $row["Folder"];
-    if ($row["IsBoxset"] == true)
-        $isboxset = true;
-    $boxsetid = $row["BoxsetId"];
 
-    if ($folder == "") {
-        continue;
-    }
-
-    $len = mb_strlen($folder);
-    $pos = mb_strrpos($folder, "/");
-    if ($albumfolder == "") {
-        $albumfolder = mb_substr($folder, $pos + 1);
-    }
-    $offset = -1 * ($len - $pos + 1);
-    $pos = mb_strrpos($folder, "/", $offset);
     $zipfolder = mb_substr($folder, $pos + 1) . "/";
-    if ($boxsetid > 0 or mb_substr($zipfolder, 0, 7) == "Singles") {
-        $offset = -1 * ($len - $pos + 1);
-        $pos = mb_strrpos($folder, "/", $offset);
-    }
-    $zipfolder = mb_substr($folder, $pos + 1) . "/";
+    $folder = mb_substr($folder, $sympos);
 
-    if ($zipfile == "") {
-        $zipfile = mb_substr($folder, $pos + 1);
-        if ($isboxset == true) {
-            $len = mb_strrpos($zipfile, "/");
-            $zipfile = mb_substr($zipfile, 0, $len);
-        }
-        $zipfile = str_replace("/", " - ", $zipfile) . ".zip";
+    if ($counter == 1) {
+        $filename = "../album.html";
+        $zipfilename = "album.html";
+        $zip->addFile($filename, $zipfolder . $zipfilename);
+        $filename = "../plugins/oyoplayer/oyoplayer.js";
+        $zipfilename = "oyoplayer.js";
+        $zip->addFile($filename, $zipfolder . $zipfilename);
+        $filename = "../plugins/oyoplayer/oyoplayer.css";
+        $zipfilename = "oyoplayer.css";
+        $zip->addFile($filename, $zipfolder . $zipfilename);
     }
 
-    $imagefilename = $symaudiosource . $folder . "/Folder.jpg";
+    $imagefilename = $symaudiosource . $folder . "/" . "Folder.jpg";
     $zipfilename = "Folder.jpg";
     $zip->addFile($imagefilename, $zipfolder . $zipfilename);
 
@@ -77,25 +91,9 @@ while ($row = mysqli_fetch_assoc($result)) {
 
     while ($row2 = mysqli_fetch_assoc($result2)) {
         $filename = $symaudiosource . $folder . "/" . $row2["FileName"];
-        fwrite($log, $filename . "\r\n");
         $zipfilename = $row2["FileName"];
         $zip->addFile($filename, $zipfolder . $zipfilename);
     }
-}
-
-if ($boxsetid > 0) {
-    $len = mb_strlen($folder);
-    $pos = mb_strrpos($folder, "/");
-    $offset = $len - $pos + 1;
-
-    $folder = mb_substr($folder, 0, $len - $offset + 1);
-    $imagefilename = $symaudiosource . $folder . "/Folder.jpg";
-
-    $len = mb_strlen($zipfolder);
-    $zipfolder = mb_substr($zipfolder, 0, $len - $offset + 1);
-
-    $zipfilename = "Folder.jpg";
-    $zip->addFile($imagefilename, $zipfolder . $zipfilename);
 }
 
 $zip->close();
@@ -123,6 +121,28 @@ unlink($tempfile);
 fclose($log);
 
 rmdir($symaudiosource);
+unlink($symaudiosource);
+
+function makeJSON() {
+    $tempfile = tempnam("../tmp", "");
+    $jsonfile = fopen($tempfile, "w");
+    $sql = "select * from artists inner join albums on artists.id=artistid where albums.id=$albumid";
+    $result = mysqli_query($link, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    $name = $row["Name"];
+
+    $released = $row["Released"];
+    $title = $row["Title"];
+    $disccount = $row["DiscCount"];
+    $formatid = $row["FormatId"];
+    $playingtime = $row["PlayingTime"];
+    $genreid = $row["GenreId"];
+    $imagefilename = $row["ImageFileName"];
+    $isboxset = $row["IsBoxset"];
+    $boxsetid = $row["BoxstId"];
+    $boxsetindex = $row["BoxsetIndex"];
+}
 
 mysqli_close($link);
 ?>
