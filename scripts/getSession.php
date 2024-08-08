3da84ddb1e854fad7f82b30ae6a1b8e7375a0298
oYoSoftware <code@oyosoftware.nl>
2024-08-08 17:07:13 +0200

    Add files via upload

--- scripts/getSession.php
+++ scripts/getSession.php
@@ -1,4 +1,14 @@
 <?php
+/*
+  oyomusic 2.2
+  tested with joomla 4.4.6
+  http://www.oyoweb.nl
+
+  © 2015-2024 oYoSoftware
+  MIT License
+
+  getSession: get login data for joomla session to be able to download albums
+  partly taken from /includes/app.php  */
 
 error_reporting(E_ERROR);
 
@@ -16,9 +26,16 @@ if (file_exists($joomlapath)) {
     require_once($joomlapath . "/includes/defines.php");
     require_once($joomlapath . "/includes/framework.php");
 
-    $app = JFactory::getApplication('site');
-    $session = JFactory::getSession();
+    $container = \Joomla\CMS\Factory::getContainer();
+    $container->alias('session.web', 'session.web.site')
+        ->alias('session', 'session.web.site')
+        ->alias('JSession', 'session.web.site')
+        ->alias(\Joomla\CMS\Session\Session::class, 'session.web.site')
+        ->alias(\Joomla\Session\Session::class, 'session.web.site')
+        ->alias(\Joomla\Session\SessionInterface::class, 'session.web.site');
+    $app = $container->get(\Joomla\CMS\Application\SiteApplication::class);
 
+    $session = \Joomla\CMS\Factory::getApplication()->getSession();
     $username = $session->get("user.username");
     $expire = $session->getExpire();
     $start = $session->get("session.timer.start") ? $session->get("session.timer.start") : 0;
