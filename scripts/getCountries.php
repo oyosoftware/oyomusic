4406edd4e08fb5d191d6df2816cdfd1a1eeabfc5
oYoSoftware <code@oyosoftware.nl>
2024-10-29 08:13:50 +0100

    Add files via upload

--- scripts/getCountries.php
+++ scripts/getCountries.php
@@ -3,6 +3,7 @@
 error_reporting(E_ERROR);
 
 require_once('../settings.inc');
+require_once('../include/oyoCountryList.php');
 
 $link = mysqli_connect($server, $username, $password, $database);
 mysqli_set_charset($link, "utf8");
@@ -10,12 +11,14 @@ mysqli_set_charset($link, "utf8");
 $sql = "select * from countries order by country";
 $result = mysqli_query($link, $sql);
 
+$countryList = new oyoCountryList("en");
 $countries = array();
 
 while ($row = mysqli_fetch_assoc($result)) {
     $country = (object) [];
     $country->id = (int) $row["Id"];
     $country->country = htmlspecialchars($row["Country"]);
+    $country->countrycode = strtolower($countryList->getCountryCode($country->country));
     $countries[] = $country;
 }
 
